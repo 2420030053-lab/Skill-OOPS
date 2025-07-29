0890e79139f350687fa87733ec0ca3aac17c03af
2420030053-lab <2420030053@klh.edu.in>
2025-07-29 14:50:38 +0530

    Add files via upload

---
+++ Narrowing_type_conversion.java
@@ -0,0 +1,14 @@
+package codes;
+
+public class Narrowing_type_conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 double doubleValue = 42.89;
+		 int intValue= (int) doubleValue;
+		 System.out.println("Original double value: " + doubleValue);
+	        System.out.println("Converted int value: " + intValue);
+
+	}
+
+}
---
+++ Widening_type_conversion.java
@@ -0,0 +1,17 @@
+package codes;
+
+public class Widening_type_conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intVal = 1234; 
+	    long longVal = intVal;      
+	    float floatVal = longVal;    
+
+	    System.out.println("Integer value: " + intVal);
+	    System.out.println("Long value: " + longVal);
+	    System.out.println("Float value: " + floatVal);
+
+	}
+
+}
